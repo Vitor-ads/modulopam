07b82323e7fc34ac1c695a78b91a39cca95cd714
Vitor-ads <vitor.marcolin.ads@gmail.com>
2021-04-01 11:35:19 -0300

    email sender criado

---
+++ sendMail.c
@@ -0,0 +1,41 @@
+#include <curl/curl.h>
+
+int main(void)
+{
+    CURLcode ret;
+    CURL *hnd;
+    struct curl_slist *recipients;
+    FILE *fd;
+    
+    recipients = NULL;
+    recipients = curl_slist_append(recipients, "vitor.marcolin.ads@gmail.com");
+    recipients = curl_slist_append(recipients, "anisio.silva01.fatec.sp.gov.br");  
+  
+    fd = fopen("mail.txt", "rb");
+    if (!fd) {return 1;} 
+
+    hnd = curl_easy_init();
+    curl_easy_setopt(hnd, CURLOPT_INFILESIZE_LARGE, (curl_off_t)179);
+    curl_easy_setopt(hnd, CURLOPT_URL, "smtps://smtp.gmail.com:465/mail.txt");
+    curl_easy_setopt(hnd, CURLOPT_UPLOAD, 1L);
+    curl_easy_setopt(hnd, CURLOPT_READDATA, fd); 
+    curl_easy_setopt(hnd, CURLOPT_USERPWD, "arch.authentication.pam2fa@gmail.com:Pam2FaRossano");
+    curl_easy_setopt(hnd, CURLOPT_USERAGENT, "curl/7.75.0");
+    curl_easy_setopt(hnd, CURLOPT_MAXREDIRS, 50L);
+    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYHOST, 0L);
+    curl_easy_setopt(hnd, CURLOPT_USE_SSL, (long)CURLUSESSL_ALL);
+    curl_easy_setopt(hnd, CURLOPT_TCP_KEEPALIVE, 1L);
+    curl_easy_setopt(hnd, CURLOPT_MAIL_FROM, "arch.authentication.pam2fa@gmail.com");
+    curl_easy_setopt(hnd, CURLOPT_MAIL_RCPT, recipients);
+    curl_easy_setopt(hnd, CURLOPT_VERBOSE, 1L);
+
+    ret = curl_easy_perform(hnd);
+
+    curl_easy_cleanup(hnd);
+    hnd = NULL;
+    curl_slist_free_all(recipients);
+    recipients = NULL;
+
+    return (int)ret;
+}
