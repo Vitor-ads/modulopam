732ce2b31dbdb3478aa619e375079f5165306cbb
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-05-07 00:51:45 -0300

    Create criar_arquivos.c

---
+++ criar_arquivos.c
@@ -0,0 +1,11 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <errno.h>
+#include <pwd.h>
